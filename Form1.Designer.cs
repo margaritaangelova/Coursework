2bf1642261ffa5a07f88c6ca5dc6898fdc5c6ab3
margaritaangelova <74078034+margaritaangelova@users.noreply.github.com>
2021-01-15 22:09:05 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,193 @@
+﻿
+namespace FloydAlg
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.loadImageToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.editToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.grayScaleToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.quantizationToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.floydSToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 45);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(425, 258);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.editToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.loadImageToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // loadImageToolStripMenuItem
+            // 
+            this.loadImageToolStripMenuItem.Name = "loadImageToolStripMenuItem";
+            this.loadImageToolStripMenuItem.Size = new System.Drawing.Size(133, 22);
+            this.loadImageToolStripMenuItem.Text = "LoadImage";
+            this.loadImageToolStripMenuItem.Click += new System.EventHandler(this.loadImageToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(133, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // editToolStripMenuItem
+            // 
+            this.editToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.grayScaleToolStripMenuItem,
+            this.quantizationToolStripMenuItem,
+            this.floydSToolStripMenuItem});
+            this.editToolStripMenuItem.Name = "editToolStripMenuItem";
+            this.editToolStripMenuItem.Size = new System.Drawing.Size(39, 20);
+            this.editToolStripMenuItem.Text = "Edit";
+            // 
+            // grayScaleToolStripMenuItem
+            // 
+            this.grayScaleToolStripMenuItem.Name = "grayScaleToolStripMenuItem";
+            this.grayScaleToolStripMenuItem.Size = new System.Drawing.Size(142, 22);
+            this.grayScaleToolStripMenuItem.Text = "GrayScale";
+            this.grayScaleToolStripMenuItem.Click += new System.EventHandler(this.grayScaleToolStripMenuItem_Click);
+            // 
+            // quantizationToolStripMenuItem
+            // 
+            this.quantizationToolStripMenuItem.Name = "quantizationToolStripMenuItem";
+            this.quantizationToolStripMenuItem.Size = new System.Drawing.Size(142, 22);
+            this.quantizationToolStripMenuItem.Text = "Quantization";
+            this.quantizationToolStripMenuItem.Click += new System.EventHandler(this.quantizationToolStripMenuItem_Click);
+            // 
+            // floydSToolStripMenuItem
+            // 
+            this.floydSToolStripMenuItem.Name = "floydSToolStripMenuItem";
+            this.floydSToolStripMenuItem.Size = new System.Drawing.Size(142, 22);
+            this.floydSToolStripMenuItem.Text = "Floyd_S";
+            this.floydSToolStripMenuItem.Click += new System.EventHandler(this.floydSToolStripMenuItem_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(618, 75);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Visible = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label1.Location = new System.Drawing.Point(615, 59);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(173, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Please enter the length of the lines:";
+            this.label1.Visible = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(615, 82);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem loadImageToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem editToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem grayScaleToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem quantizationToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem floydSToolStripMenuItem;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FloydAlg
+{
+    public partial class Form1 : Form
+    {
+        private Bitmap inBm;
+        private Bitmap outBm;
+        private int width;
+        private int height;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void loadImageToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+
+            {
+
+                inBm = new Bitmap(Image.FromFile(openFileDialog1.FileName));
+
+                width = inBm.Width;
+
+                height = inBm.Height;
+
+                pictureBox1.Size = new Size(width, height);
+
+                pictureBox1.Image = inBm;
+
+                textBox1.Visible = true;
+                label1.Visible = true;
+                
+
+
+            }
+        }
+
+        private void grayScaleToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < width; i++)
+
+                for (int j = 0; j < height; j++)
+
+                {
+
+                    int c = inBm.GetPixel(i, j).R;
+
+                    Color r = Color.FromArgb(c, c, c);
+
+                    inBm.SetPixel(i, j, r);
+
+                }
+
+            pictureBox1.Image = inBm;
+        }
+
+        private int Approximate(int c)
+
+        {
+
+            if (c >= 0 && c < 63)
+
+                return 0;
+
+            if (c >= 63 && c < 127)
+
+                return 63;
+
+            if (c >= 127 && c < 191)
+
+                return 127;
+
+            if (c >= 191 && c < 255)
+
+                return 191;
+
+            else
+
+                return 255;
+
+        }
+
+        private void quantizationToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            outBm = new Bitmap(width, height);
+
+            for (int i = 0; i < width; i++)
+
+                for (int j = 0; j < height; j++)
+
+                {
+
+                    int c = inBm.GetPixel(i, j).R;
+
+                    int d = Approximate(c);
+
+                    outBm.SetPixel(i, j, Color.FromArgb(d, d, d));
+
+                }
+
+            pictureBox1.Image = outBm;
+        }
+
+        private void floydSToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            int r, r1, r2, r3, r4;
+            int error;
+            outBm = new Bitmap(width, height);
+
+            string mString = textBox1.Text;
+            int m = Int32.Parse(mString);
+
+
+            for (int i = width - 2; i > 0; i--)
+                for (int j = 1; j < height - 1; j++)
+                {
+                    int c = inBm.GetPixel(i, j).R;
+                    r = Approximate(c);
+
+                    if(r==63 || r==127 || r==191 || r==255)
+                    {
+                        Pen pen = new Pen(Color.FromArgb(r, r, r));
+                        using(var graphics = Graphics.FromImage(outBm))
+                        {
+                            graphics.DrawLine(pen,
+                            i,
+                            j,
+                            i + m,
+                            j + m);
+                        }
+
+                        error = c - r - (m-1);
+                    }
+                    else
+                    {
+                        error = c;
+                    }
+                    outBm.SetPixel(i, j, Color.FromArgb(r, r, r));
+                    //error = c - r;
+
+                    r1 = inBm.GetPixel(i, j + 1).R + 7 * error / 16;
+                    r2 = inBm.GetPixel(i + 1, j - 1).R + 3 * error / 16;
+                    r3 = inBm.GetPixel(i + 1, j).R + 5 * error / 16;
+                    r4 = inBm.GetPixel(i + 1, j + 1).R + error / 16;
+
+                    if (r1 >= 255) r1 = 255;
+                    if (r2 >= 255) r2 = 255;
+                    if (r3 >= 255) r3 = 255;
+                    if (r4 >= 255) r4 = 255;
+
+                    if (r1 < 0) r1 = 0;
+                    if (r2 < 0) r2 = 0;
+                    if (r3 < 0) r3 = 0;
+                    if (r4 < 0) r4 = 0;
+
+                    outBm.SetPixel(i, j + 1, Color.FromArgb(r1, r1, r1));
+                    outBm.SetPixel(i + 1, j - 1, Color.FromArgb(r2, r2, r2));
+                    outBm.SetPixel(i + 1, j, Color.FromArgb(r3, r3, r3));
+                    outBm.SetPixel(i + 1, j + 1, Color.FromArgb(r4, r4, r4));
+                }
+            pictureBox1.Image = outBm;
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+    }
+}
